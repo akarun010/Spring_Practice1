90a2c27338051517a4695ca9950255e2e299a5d7
Arun <akarunff0@gmail.com>
2026-09-11 16:10:39 +0530

    New Spring Boot Practice Repo

---
+++ src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Hello {
+    public void greet(){
+        System.out.println("Hello World");
+    }
+}
